6f026ca6868f2a8ad9304e45b92b9ab7b39482f2
Benjamin <bcruz@asdeporte.com>
2021-03-20 20:57:22 -0600

    connection to service prod

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
-import { validateFilds, messageAlert} from '../helpers';
-import '../assets/styles/App.scss';
+import axios from 'axios';
+import CreditCard from './credit-card';
+import { validateFilds, messageAlert } from '../helpers';
+
+import config from '../../config.json';
 
 const App = () => {
-    const [data, setData] = useState({});
-    const [cvv, setCvv] = useState({ showCvv: false });
+    const [loadSubmit, setLoadSubmit] = useState(false);
 
-    const onFocus = () => {
-        setCvv({ showCvv: true });
-    };
-
-    const onFocusOut = () => {
-        setCvv({ showCvv: false });
-    };
-  
-    const handleChange = event => {
-        const { name, value } = event.target;
-        const dataForm = Object.assign({}, data);
-        dataForm[name] = value;
-        setData(dataForm);
-    };
-
-    const handleSubmit = () => {
+    const handleSubmit = data => {
         const dataForm = Object.assign({}, data);
         const dataValidate = [];
         dataValidate.push(dataForm.cardNumber);
@@ -29,79 +16,38 @@ const App = () => {
         dataValidate.push(dataForm.month);
         dataValidate.push(dataForm.year);
         dataValidate.push(dataForm.cvvValue);
-        validateFilds(dataValidate, valid => {
+        validateFilds (dataValidate, async(valid) => {
             if (valid) {
-                messageAlert({ type: 'success', title: 'Todo chido' });
+                const dataSend = {
+                    cardNumber: dataForm.cardNumber,
+                    cardName: dataForm.cardName,
+                    cvv: dataForm.cvvValue,
+                    expiration: `${dataForm.month}/${dataForm.year}`
+                };
+                setLoadSubmit(true);
+                axios.post(`${config.api}/api/v1/card`, dataSend, { headers: {}})
+                    .then(response => {
+                        setLoadSubmit(false);
+                        messageAlert({ type: 'info', title: response.data.message });
+                    })
+                    .catch((error) => {
+                        setLoadSubmit(false);
+                        messageAlert({ type: 'info', title: error.response.data.message });
+                    });
             } else {
                 messageAlert({ type: 'info', title: 'Faltan campos requeridos' });
             }
         });
     };
     
-    const { cardNumber, cardName, month, year, cvvValue } = data;
+    
     return (
-        <div className="App">
-            <div className="card">
-                <div className={`credit-card ${cvv.showCvv ? 'active' : ''}`}>
-                    <div className="card-1">
-                        <p className="num-card">
-                            {cardNumber ? cardNumber.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ') : ''}
-                        </p>
-                        <p className="expired">{ month ? month : ''}/{ year ? year : ''}</p>
-                        <p className="name">{ cardName ? cardName.trim() : '' }</p>
-                    </div>
-                    <div className="card-2">
-                        <p className="cvv">{cvvValue ? cvvValue : ''}</p>
-                    </div>
-                </div>
-                <form autoComplete={'off'}>
-                    <div className="cont-input">
-                        <label>Card Number</label>
-                        <input type="number" name="cardNumber"
-                            value={cardNumber ? cardNumber : ''}
-                            onChange={handleChange}
-                        />
-                    </div>
-
-                    <div className="cont-input">
-                        <label>Card Name</label>
-                        <input type="text" name="cardName"
-                            value={cardName ? cardName : ''}
-                            onChange={handleChange} />
-                    </div>
-
-                    <div className="complementary">
-                        <div className="cont-input">
-                            <label htmlFor="">Month</label>
-                            <input type="text" name={'month'}
-                                value={month ? month : ''}
-                                onChange={event => (event.target.value.length < 3) ? handleChange(event) : null}
-                            />
-                        </div>
-
-                        <div className="cont-input">
-                            <label htmlFor="">Year</label>
-                            <input type="text" name={'year'}
-                                value={year ? year : ''}
-                                onChange={event => (event.target.value.length < 5) ? handleChange(event) : null}
-                            />
-                        </div>
-
-                        <div className="cont-input">
-                            <label htmlFor="">Cvv</label>
-                            <input type="number" name="cvvValue"
-                                value={cvvValue ? cvvValue : ''}
-                                onChange={event => (event.target.value.length < 5) ? handleChange(event) : null}
-                                onFocus={onFocus}
-                                onBlur={onFocusOut}
-                            />
-                        </div>
-                    </div>
-
-                    <button className="submit" type="button" onClick={handleSubmit}>Submit</button>
-                </form>
-            </div>
-        </div>
+        <>
+            <CreditCard
+                loading={loadSubmit}
+                onSubmit={handleSubmit}
+            />
+        </>
     );
 };
 
---
+++ src/components/credit-card/index.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import '../../assets/styles/App.scss';
+
+const CreditCard = ({ onSubmit, loading }) => {
+    const [data, setData] = useState({});
+    const [cvv, setCvv] = useState({ showCvv: false });
+
+    const onFocus = () => {
+        setCvv({ showCvv: true });
+    };
+
+    const onFocusOut = () => {
+        setCvv({ showCvv: false });
+    };
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        const dataForm = Object.assign({}, data);
+        dataForm[name] = value;
+        setData(dataForm);
+    };
+  
+    const { cardNumber, cardName, month, year, cvvValue } = data;
+
+    return (
+        <div className="App">
+            <div className="card">
+                <div className={`credit-card ${cvv.showCvv ? 'active' : ''}`}>
+                    <div className="card-1">
+                        <p className="num-card">
+                            {cardNumber ? cardNumber.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ') : ''}
+                        </p>
+                        <p className="expired">{month ? month : ''}/{year ? year : ''}</p>
+                        <p className="name">{cardName ? cardName.toUpperCase().trim() : ''}</p>
+                    </div>
+                    <div className="card-2">
+                        <p className="cvv">{cvvValue ? cvvValue : ''}</p>
+                    </div>
+                </div>
+                <form autoComplete={'off'}>
+                    <div className="cont-input">
+                        <label>Card Number</label>
+                        <input type="number" name="cardNumber"
+                            value={cardNumber ? cardNumber : ''}
+                            onChange={event => (event.target.value.length <= 16) ? handleChange(event) : null}
+                        />
+                    </div>
+
+                    <div className="cont-input">
+                        <label>Card Name</label>
+                        <input type="text" name="cardName"
+                            value={cardName ? cardName.toUpperCase() : ''}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="complementary">
+                        <div className="cont-input">
+                            <label htmlFor="">Month</label>
+                            <input type="text" name={'month'}
+                                value={month ? month : ''}
+                                onChange={event => (event.target.value.length < 3) ? handleChange(event) : null}
+                            />
+                        </div>
+
+                        <div className="cont-input">
+                            <label htmlFor="">Year</label>
+                            <input type="text" name={'year'}
+                                value={year ? year : ''}
+                                onChange={event => (event.target.value.length < 5) ? handleChange(event) : null}
+                            />
+                        </div>
+
+                        <div className="cont-input">
+                            <label htmlFor="">Cvv</label>
+                            <input type="number" name="cvvValue"
+                                value={cvvValue ? cvvValue : ''}
+                                onChange={event => (event.target.value.length < 5) ? handleChange(event) : null}
+                                onFocus={onFocus}
+                                onBlur={onFocusOut}
+                            />
+                        </div>
+                    </div>
+
+                    <button className="submit" type="button" disabled={loading} onClick={() => onSubmit(data)}>Submit</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+CreditCard.defaultProps = {
+    onSubmit: f => f,
+    loading: false
+};
+
+CreditCard.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    loading: PropTypes.bool
+};
+
+export default CreditCard;
